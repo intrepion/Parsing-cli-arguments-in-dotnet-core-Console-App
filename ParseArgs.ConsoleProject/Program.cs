f870f9bba1828c3419e0df590cf431338ca855eb
Oliver Forral <intrepion@gmail.com>
2023-04-07 23:57:05 -0600

    Added a command to list and a command to add a catapult.

--- ParseArgs.ConsoleProject/Program.cs
+++ ParseArgs.ConsoleProject/Program.cs
@@ -1,7 +1,38 @@
 ﻿using Microsoft.Extensions.CommandLineUtils;
 
 var app = new CommandLineApplication();
-app.Command("catapult", config => { });
+var catapult = app.Command("catapult", config =>
+{
+
+});
+catapult.Command("list", config =>
+{
+    config.OnExecute(() =>
+    {
+
+        config.Description = "list catapults";
+        Console.WriteLine("a");
+        Console.WriteLine("b");
+        return 0;
+    });
+});
+catapult.Command("add", config =>
+{
+    config.Description = "Add a catapult";
+    var arg = config.Argument("name", "name of the catapult", false);
+    config.OnExecute(() =>
+    {
+        if (!string.IsNullOrWhiteSpace(arg.Value))
+        {
+            //add snowballs somehow (not showing persistence here)
+            Console.WriteLine($"added {arg.Value}");
+            return 0;
+        }
+        return 1;
+
+
+    });
+});
 app.Command("snowball", config => { });
 //give people help with --help
 app.HelpOption("-? | -h | --help");
