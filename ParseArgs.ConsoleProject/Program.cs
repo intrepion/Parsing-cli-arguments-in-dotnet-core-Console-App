250e425bc670a01a9cf156f60d0b87adb1d28e64
Oliver Forral <intrepion@gmail.com>
2023-04-08 11:12:37 -0600

    Added add and list to snowballs.

--- ParseArgs.ConsoleProject/Program.cs
+++ ParseArgs.ConsoleProject/Program.cs
@@ -47,7 +47,52 @@ catapult.Command("add", config =>
 
     });
 });
-app.Command("snowball", config => { });
+var snowball = app.Command("snowball", config =>
+{
+    config.OnExecute(() =>
+    {
+        config.ShowHelp(); //show help for catapult
+        return 0; //return error since we didn't do anything
+    });
+    config.HelpOption("-? | -h | --help"); //show help on --help
+});
+snowball.Command("help", config =>
+{
+    config.Description = "get help!";
+    config.OnExecute(() =>
+    {
+        catapult.ShowHelp("snowball");
+        return 1;
+    });
+});
+snowball.Command("list", config =>
+{
+    config.Description = "list snowballs";
+    config.OnExecute(() =>
+    {
+
+        Console.WriteLine("1");
+        Console.WriteLine("2");
+        return 0;
+    });
+});
+snowball.Command("add", config =>
+{
+    config.Description = "Add a snowball";
+    var arg = config.Argument("name", "name of the snowball", false);
+    config.OnExecute(() =>
+    {
+        if (!string.IsNullOrWhiteSpace(arg.Value))
+        {
+            //add snowballs somehow
+            Console.WriteLine($"added {arg.Value}");
+            return 0;
+        }
+        return 0;
+
+
+    });
+});
 //give people help with --help
 app.HelpOption("-? | -h | --help");
 app.Execute(args);
